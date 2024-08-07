05ef346ea5f09756df9c3c68e68511790302994d
Lindsey Feng <lindseyf@Lindseys-Air.attlocal.net>
2024-08-07 12:16:45 -0400

    switch phi mode to qwen1.5

--- rewardbench/models/__init__.py
+++ rewardbench/models/__init__.py
@@ -47,8 +47,8 @@ from .modeling_phi3 import Phi3ForSequenceClassification
 # Please open a PR if you need to add more custom modeling code / utilize existing code for you model
 REWARD_MODEL_CONFIG = {
     "default": {
-        # "model_builder": AutoModelForSequenceClassification.from_pretrained,
-        "model_builder": Phi3ForSequenceClassification.from_pretrained,
+        "model_builder": AutoModelForSequenceClassification.from_pretrained,
+        # "model_builder": Phi3ForSequenceClassification.from_pretrained,
         "pipeline_builder": RewardBenchPipeline,
         "quantized": True,
         "custom_dialogue": False,
